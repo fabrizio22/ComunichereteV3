7e19e475421e3003163620ae9ab83fb69533addd
fabrizio22 <pmcomunicherete@gmail.com>
2014-02-14 17:18:24 +0100

    commit 14_02_2014 ore 17_18

--- wp-content/themes/vantage/page-form-governance.php
+++ wp-content/themes/vantage/page-form-governance.php
@@ -1,121 +1,135 @@
-	<?php
+<?php
 	/**
-	 * The template for displaying all pages.
-	 *
-	 * This is the template that displays all pages by default.
-	 * Please note that this is the WordPress construct of pages
-	 * and that other 'pages' on your WordPress site will use a
-	 * different template.
-	 *
-	 * @package alexandria
-	 */
+     * The template for displaying all pages.
+     *
+     * This is the template that displays all pages by default.
+     * Please note that this is the WordPress construct of pages
+     * and that other 'pages' on your WordPress site will use a
+     * different template.
+     *
+     * @package alexandria
+     */
+    global $wpdb;
+    $bp_prefix       = bp_core_get_table_prefix();
 
-/*
-	if (isset($_POST['SubComuni'])){
-		$comune = $_POST['comune'];
-	    echo($comune);
-	}
-
-
-
-	if (isset($_POST['procedi_registrazione'])){
-
-
-	    $risultatiQuery = array();
-	    $nome = $_POST['nome'];
-	    $cognome = $_POST['cognome'];
-	 
-		if((!empty($nome)) && (!empty($cognome)) && (!empty($mail))){ 		  
-			global $wpdb;
-			
-		    //$risultatiQuery =  registrazioni( $nome );
-		    $risultatiQuery = $wpdb->get_row( $wpdb->prepare( "select * from dati_utente_registrazione where nome = %s AND cognome  = %s", $nome, $cognome ) );
-		    
-		    if ( is_object( $risultatiQuery ) ){
-				$nome_valore = $risultatiQuery->nome;
-				$cognome_valore = $risultatiQuery->cognome;
-				$mail_valore = $risultatiQuery->mail;	
-				$titoloAccademico_valore = $risultatiQuery->titolo_accademico;
-				
-				//echo($nome_valore);
-				//echo($cognome_valore);
-				//echo($mail_valore);		
-		    }else{
-			    $errore = 'dati non presenti nel db';
-		    }
-		}
-		else
-		{
-			$errore = 'inserire i campi del form';
-			
-		} 
-	}
-
-*/
 	if (isset($_POST['Submit'])){
 
-		//echo('sono nel salva');
+        //echo('sono nel salva');
 
 
-		$cognome_valore = $_POST['cognome'];
-		$nome_valore = $_POST['nome'];
-		$comune = $_POST['comune'];
-		$mail = $_POST['mailIns'];
-		$sezAmministrativo = $_POST['sezAmministrativo'];
-		$sezSettori = $_POST['sezSettori'];
-		$indirizzoMail = $_POST['indirizzoMail'];
+        $cognome_valore = $_POST['cognome'];
+        $nome_valore = $_POST['nome'];
+        $secondoNome_valore = $_POST['secondoNome'];
+        $registrazione1_valore = $_POST['registrazione1'];
+        $comune = $_POST['comune'];
+        $mail = $_POST['mailIns'];
+        $indirizzoMail = $_POST['indirizzoMail'];
+        $sezOrgAppatenenza = $_POST['sezOrgAppatenenza'];
+        $sezAmministrativo = $_POST['sezAmministrativo'];
+        $sezSettori = $_POST['sezSettori'];
 
-		$mailValida =$mail . $indirizzoMail;
+
+        // domande si sicurezza
+        $password = $_POST['password'];
+        $selectDomanda1 = $_POST['selectDomanda1'];
+        $risposta1 = $_POST['risposta1'];
+        $selectDomanda1 = $_POST['selectDomanda2'];
+        $risposta1 = $_POST['risposta2'];
+        $selectDomanda1 = $_POST['selectDomanda3'];
+        $risposta1 = $_POST['risposta3'];
 
 
-		$password = '123456';
-		
-		$user_data = array(
-		'ID' => '',
-		'user_pass' => $password,
-		'user_login' => $nome_valore
-		);
+        //organo di controllo
+        $sezOganoControllo = $_POST['sezOganoControllo'];
 
-		$new_userid = wp_insert_user( $user_data );
-		
-		if( isset($new_userid)  ) {
+//        //deleghe
+//        $deleghe = $_POST['deleghe'];
+//        $partitoPolitico = $_POST['partitoPolitico'];
+//        $dataElezioneGG = $_POST['dataElezioneGG'];
+//        $dataElezioneMM = $_POST['dataElezioneMM'];
+//        $dataElezioneAA = $_POST['dataElezioneAA'];
+//
+//        $dataElezione = $dataElezioneGG."-".$dataElezioneMM."-".$dataElezioneAA;
+//
+//        $dataNominaGG = $_POST['dataNominaGG'];
+//        $dataNominaMM = $_POST['dataNominaMM'];
+//        $dataNominaAA = $_POST['dataNominaAA'];
+//
+//        $dataNomina = $dataNominaGG."-".$dataNominaMM."-".$dataNominaAA;
 
-		} else {
-			 $errore = 'Error on user creation';
-		}
-		
-		$corpoMail = "e stata eggettuata una registrazione con il seguente nome" .$nome_valore . " e cognome " . $cognome_valore ." i dati di accesso sono :" . $nome_valore . "e pass" . $password;
 
-		mail($mailValida, "invio mail registrazione", $corpoMail, "From: ".$mailValida);
-		
-		wp_redirect(get_bloginfo('url').'/wp-content/themes/vantage/page-registrazione.php');
-				
-	}
+        //uffici
+        $suap = $_POST['suap'];
+        $ufficioAnagrafe = $_POST['ufficioAnagrafe'];
+        $culturaSportTurismo = $_POST['culturaSportTurismo'];
+        $economato = $_POST['economato'];
+        $ufficioElettorale = $_POST['ufficioElettorale'];
+        $ufficioGiuntaComunale = $_POST['ufficioGiuntaComunale'];
+        $ufficioLavoriPubblici = $_POST['ufficioLavoriPubblici'];
+        $ufficioProtocollo = $_POST['ufficioProtocollo'];
+        $ufficioRelazioniPubblico = $_POST['ufficioRelazioniPubblico'];
+        $ufficioSegreteriaGenerale = $_POST['ufficioSegreteriaGenerale'];
+        $suap = $_POST['ufficioServiziDemografici'];
+        $ufficioServiziSociali = $_POST['ufficioServiziSociali'];
+
+
+        $mailValida =$mail . $indirizzoMail;
+
+
+        //$password = '123456';
+
+        $user_data = array(
+            'ID' => '',
+            'user_pass' => $password,
+            'user_login' => $nome_valore
+        );
+
+        $new_userid = wp_insert_user( $user_data );
+
+        if( isset($new_userid)  ) {
+
+
+            $profile_fieldsId_Name = $wpdb->get_results( "SELECT id, name FROM {$bp_prefix}bp_xprofile_fields", ARRAY_A);
+
+
+
+
+            $corpoMail = "e stata eggettuata una registrazione con il seguente nome" .$nome_valore . " e cognome " . $cognome_valore ." i dati di accesso sono :" . $nome_valore . "e pass" . $password;
+
+            mail($mailValida, "invio mail registrazione", $corpoMail, "From: ".$mailValida);
+            wp_redirect(get_bloginfo('url').'/wp-content/themes/vantage/page-registrazione.php');
+
+
+        } else {
+            $errore = 'Error on user creation';
+        }
+
+    }
 
 
 	get_header(); ?>
 
 
-				<label class="titoloSezione">Registrati</label><br /><hr/>
+<label class="titoloSezione">Registrati</label><br /><hr/>
 
-				<form id="form_organoPolitico" class="form_organoPolitico" name="form_organoPolitico" method="post" action="<?php echo home_url( 'form-governance' ); ?>">
-				<?php include('include/governance/form-registrazioneIniziale.php'); ?>
-				<?php include('include/governance/form-comune.php'); ?>
-				<?php include('include/governance/form-personaleAmministrativi.php'); ?>
-				<?php include('include/governance/form-organoGoverno.php'); ?>
-				<?php include('include/governance/form-organiControllo.php'); ?>
-<!-- 				<?php include('include/form-personaleAmministrativi.php'); ?> -->
-				<?php include('include/governance/form-UfficioAnagrafe.php'); ?>
-				<?php include('include/governance/form-deleghe.php'); ?>
-				<?php include('include/governance/form-settori.php'); ?>
-				<?php include('include/form-domandeSicurezza.php'); ?>
-				<?php include('include/form-Privacy-accettazione.php'); ?>
-				
-				
-				<div class="salvaButton" id="salvaButton">
-					<input id="salva" type="submit" name="Submit" value="Submit" tabindex="2" />
-				</div>
-				</form>	
-	</div>
+<form id="form_organoPolitico" class="form_organoPolitico" name="form_organoPolitico" method="post" action="<?php echo home_url( 'form-governance' ); ?>">
+    <?php include('include/governance/form-registrazioneIniziale.php'); ?>
+    <?php include('include/governance/form-comune.php'); ?>
+    <?php include('include/governance/form-personaleAmministrativi.php'); ?>
+    <?php include('include/governance/form-organoGoverno.php'); ?>
+    <?php include('include/governance/form-organiControllo.php'); ?>
+    <!-- 				<?php include('include/form-personaleAmministrativi.php'); ?> -->
+    <?php include('include/governance/form-UfficioAnagrafe.php'); ?>
+    <?php include('include/governance/form-deleghe.php'); ?>
+    <?php include('include/governance/form-settori.php'); ?>
+    <?php include('include/form-domandeSicurezza.php'); ?>
+    <?php include('include/form-Privacy-accettazione.php'); ?>
 
-	<?php get_footer(); ?>
+
+    <div class="salvaButton" id="salvaButton">
+        <input id="salva" type="submit" name="Submit" value="Submit" tabindex="2" />
+    </div>
+</form>
+</div>
+
+<?php get_footer(); ?>
